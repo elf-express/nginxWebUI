f77e8ea78bdd8eeed53f5246dcf5c407371a2edf
楊清雲 <41539966+TW199501@users.noreply.github.com>
2026-07-10 05:19:07 +0800

    feat(geoip): JS downloadCloudflare + reverifyAll

--- src/main/resources/static/js/adminPage/protectionCert/geoip.js
+++ src/main/resources/static/js/adminPage/protectionCert/geoip.js
@@ -29,5 +29,44 @@ var geoipNS = {};
 		});
 	}
 
+	function downloadCloudflare() {
+		layer.confirm(geoipStr.download + ' (Cloudflare) ?', { icon: 3 }, function(idx) {
+			layer.close(idx);
+			var loading = layer.msg(geoipStr.downloading, { icon: 16, time: 0, shade: 0.1 });
+			$.ajax({
+				type: 'POST',
+				url: ctx + '/adminPage/geoip/downloadCloudflare',
+				dataType: 'json',
+				success: function(data) {
+					layer.close(loading);
+					if (data.success) {
+						layer.msg(geoipStr.downloadSuccess, { icon: 1 });
+						setTimeout(function() { location.reload(); }, 1000);
+					} else {
+						layer.msg(data.msg || geoipStr.downloadFail, { icon: 2 });
+					}
+				},
+				error: function() { layer.close(loading); layer.msg(geoipStr.downloadFail, { icon: 2 }); }
+			});
+		});
+	}
+
+	// 重新驗證全部：重拉 versions JSON 後 reload，即時反映最新 stat/status（不需重下載）
+	function reverify() {
+		var loading = layer.msg(geoipStr.reverifying, { icon: 16, time: 0, shade: 0.1 });
+		$.ajax({
+			type: 'GET',
+			url: ctx + '/adminPage/geoip/versions',
+			dataType: 'json',
+			success: function() {
+				layer.close(loading);
+				location.reload();
+			},
+			error: function() { layer.close(loading); layer.msg(commonStr.errorInfo, { icon: 2 }); }
+		});
+	}
+
 	ns.download = download;
+	ns.downloadCloudflare = downloadCloudflare;
+	ns.reverify = reverify;
 })(geoipNS);
